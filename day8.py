c3f2118199044228f7c7f8a85384ff96b672d61e
Alina Bendt <alina.bendt@gmail.com>
2023-12-10 14:26:17 +0000

    solves day 8 part 1

--- day8.py
+++ day8.py
@@ -1,6 +1,7 @@
 import parse
 
-with open('/home/space/phrpzz/aoc_inputs/input8.txt', 'r') as file:
+with open('/home/alina/aoc_inputs/input8.txt', 'r') as file:
+# with open('/home/space/phrpzz/aoc_inputs/input8.txt', 'r') as file:
     lines = file.readlines()
 
 instruct = lines[0].strip()
@@ -25,7 +26,54 @@ def line_dict(lin, pattern):
 
 # construct network from nodes
 # dictionary? each node is a key with left and right options
+nodes = {}
 # find node AAA as start point and follow instruct to navigate
 for line in lines[2:]:
-    line_d = line_dict(line, "{node:w} = ({right} , {left})\n")
-    print(line_d)
+    line = line.strip()
+    line_d = line_dict(line, '{node} = ({left}, {right})')
+    nodes[line_d['node']] = line_d
+x = 0
+steps = 0
+current = nodes['AAA']
+while current != nodes['ZZZ']:
+    steps += 1
+    if x>=len(instruct):
+        x = 0
+    if instruct[x]=='R':
+        current = nodes[current['right']]
+    else:
+        current = nodes[current['left']]
+    x += 1
+print('part1:', steps)
+
+# part 2
+# follow every node that ends in A until every path simultaneaously is at a node that ends in Z
+# replace all starting letters with numbers for nodes
+all_a = {}
+for node in nodes.keys():
+    if node.endswith('A'):
+        all_a[node] = nodes[node]
+current = all_a
+steps2 = 0
+x = 0
+ends = [True]
+while any(ends):
+    n_current = {}
+    ends = []
+    steps2 += 1
+    if x>=len(instruct):
+        x = 0
+    for node in current.keys():
+        if instruct[x]=='R':
+            nn = current[node]['right']
+            n_current[nn] = nodes[nn]
+        else:
+            nn = current[node]['left']
+            n_current[nn] = nodes[nn]
+        if nn.endswith('Z'):
+            ends.append(False)
+        else:
+            ends.append(True)
+    current = n_current
+    x += 1
+print('part2:', steps2)
